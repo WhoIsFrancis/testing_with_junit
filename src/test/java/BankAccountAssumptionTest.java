ed9b4279d33c227fa6b78e3bbb60925a676c7488
Francisco Passalacqua <francisco_passalacqua@epam.com>
2025-02-28 16:00:06 -0300

    Creation of different classes of the module number 3

---
+++ src/test/java/BankAccountAssumptionTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assume.assumeTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+
+public class BankAccountAssumptionTest {
+    @Test
+    @DisplayName("Test Activation account after creation")
+    public void testActive() {
+        BankAccount bankAccount = new BankAccount(500, 0);
+//        Assumptions.assumeTrue(bankAccount == null, "Account is null");
+//        assertTrue(bankAccount.isActive());
+        assumingThat(bankAccount != null, () -> assertTrue(bankAccount.isActive()));
+
+    }
+}
